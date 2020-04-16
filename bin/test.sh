952bd2721fb1db5d63465771c3a0a11c5a47a905
Seandon Mooy <seandon.mooy@gmail.com>
2020-04-16 16:35:17 -0700

    cleanup tests a bit more aggressively

--- bin/test.sh
+++ bin/test.sh
@@ -8,7 +8,9 @@ function finish {
 trap finish ERR
 
 git clean -xdf test/
+git checkout -- test/*/package.json
 
 ./node_modules/.bin/mocha ./test/index.js
 
 git clean -xdf test/ > /dev/null
+git checkout -- test/*/package.json
